01c7bffffe51cabd69da8c8ebee0c85a757be5bf
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-08 23:49:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
+let n=5;
 const divide = (arr, n) => {
   // Write your code here
+	let MaxSumArr = [];
+	let SumArr = [];
+	let sum = 0;
+	for (let i = 0; i < arr.length;) {
+	 let j=i;
+		while (sum<=n) {
+		sum+=arr[j];
+		SumArr.push(arr[j]);
+		//console.log("ghj",SumArr,"bjhbjhb", i,j,"rtyui",sum,);	
+		j++
+		i++
+	 }
+	SumArr.pop();
+	MaxSumArr.push(SumArr);
+	//console.log("maxsumarr:",MaxSumArr);
+	sum=0;
+	SumArr=[];
+	i=i-1;
+	j=j-1;	
+	}
+	return MaxSumArr;
 };
+console.log(divide(arr,n));
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
